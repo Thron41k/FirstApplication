515e5b92fda14a9a5a530458e242ad23872411bd
Timothy <Thron41k@gmail.com>
2024-09-08 07:08:26 +0800

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿var data = new string?[3];
+Console.WriteLine("Как Вас зовут?");
+data[0] = Console.ReadLine();
+Console.WriteLine("Откуда вы?");
+data[1] = Console.ReadLine();
+Console.WriteLine("Сколько вам лет?");
+data[2] = Console.ReadLine();
+Console.WriteLine($"Здравствуйте, {data[0]}! Вам {data[2]} года, вы из {data[1]}.");
+
